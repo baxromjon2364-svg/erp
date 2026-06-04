4d653ece030c53d3b99b5c14c31f69a84713c5fe
Ahrorbek <baxromjon2364@gmail.com>
2026-06-04 19:50:23 +0500

    build script oxirgi versiya

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py migrate
+
+python manage.py collectstatic --no-input
+
+# Parol bilan birga yaratish:
+DJANGO_SUPERUSER_PASSWORD='ahrorbek123456789' python manage.py createsuperuser --no-input --username=admin --email=admin@gmail.com
