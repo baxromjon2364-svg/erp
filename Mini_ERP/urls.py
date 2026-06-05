e4ff3775de54e39396e7ad041660b922390bbcac
Ahrorbek <baxromjon2364@gmail.com>
2026-06-05 13:29:43 +0500

    swagger uchun

--- Mini_ERP/urls.py
+++ Mini_ERP/urls.py
@@ -35,10 +35,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token',TokenObtainPairView.as_view(), name='api/token'),
+    path('api/token/',TokenObtainPairView.as_view(), name='api/token'),
     path('api/token/refresh/',TokenRefreshView.as_view(),name='api/token/refresh'),
     path('',include('blog.urls')),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
